4b521d4e6faf6db15c0af6fcd789e064ffaa6b30
lloppy <ankudinovapol@gmail.com>
2023-02-10 19:43:28 +0500

    create layout + connect viewBinding

--- app/src/main/java/com/example/countriesretrofit/MainActivity.kt
+++ app/src/main/java/com/example/countriesretrofit/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.countriesretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.countriesretrofit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
