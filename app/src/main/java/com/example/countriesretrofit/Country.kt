3ceaddb6105fc52fda85145e973479e82af1134a
lloppy <ankudinovapol@gmail.com>
2023-02-10 20:15:09 +0500

    connect to Retrofit

---
+++ app/src/main/java/com/example/countriesretrofit/Country.kt
@@ -0,0 +1,14 @@
+package com.example.countriesretrofit
+
+data class Country(
+    val name: String,
+    val capital: String,
+    val population: Long,
+    val area: Long,
+    val languages: List<Language>
+)
+
+
+data class Language(
+    val name: String
+)
---
+++ app/src/main/java/com/example/countriesretrofit/RemoteDataSource.kt
@@ -0,0 +1,20 @@
+package com.example.countriesretrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface RestCountriesService {
+    @GET("name/{name}\n")
+    fun getCountryByName(@Path("name") cityName: String): List<Country>
+}
+
+var retrofit: Retrofit = Retrofit.Builder()
+    .baseUrl("https://restcountries.com/v3.1/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+
+var restCountriesService: RestCountriesService = retrofit.create(RestCountriesService::class.java)
