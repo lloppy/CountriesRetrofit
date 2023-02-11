7d1bcc56a1b48d08562c745fb40dc04cd9944c4b
lloppy <ankudinovapol@gmail.com>
2023-02-11 15:53:23 +0500

    provide to text

--- app/src/main/java/com/example/countriesretrofit/MainActivity.kt
+++ app/src/main/java/com/example/countriesretrofit/MainActivity.kt
@@ -11,5 +11,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.searchButton.setOnClickListener{
+            val countryName = binding.countryNameEt.text.toString()
+
+            val countries = restCountriesService.getCountryByName(countryName)
+            val country = countries[0]
+
+            binding.countryName.text = country.name
+            binding.capitalTv.text = country.capital
+            binding.populationTv.text = country.population.toString()
+            binding.area.text = country.area.toString()
+            binding.languages.text = country.languages.toString()
+
+        }
     }
 }
--- app/src/main/java/com/example/countriesretrofit/RemoteDataSource.kt
+++ app/src/main/java/com/example/countriesretrofit/RemoteDataSource.kt
@@ -5,7 +5,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-
 interface RestCountriesService {
     @GET("name/{name}\n")
     fun getCountryByName(@Path("name") cityName: String): List<Country>
@@ -15,6 +14,5 @@ var retrofit: Retrofit = Retrofit.Builder()
     .baseUrl("https://restcountries.com/v3.1/")
     .addConverterFactory(GsonConverterFactory.create())
     .build()
-
 
 var restCountriesService: RestCountriesService = retrofit.create(RestCountriesService::class.java)
